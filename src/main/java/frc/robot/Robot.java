ad99acd29a660ab0cd33d6410eb692f18fe81890
Kerol <102901360+Kerol-Dev@users.noreply.github.com>
2024-03-12 14:20:01 +0300

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -2,6 +2,7 @@ package frc.robot;
 
 import org.littletonrobotics.junction.LoggedRobot;
 import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.GenericHID.RumbleType;
@@ -25,6 +26,7 @@ public class Robot extends LoggedRobot {
   @Override
   public void robotInit() {
     Logger.recordMetadata("Flare 2024", "Main");
+    Logger.addDataReceiver(new WPILOGWriter("/home/lvuser/logs"));
     new PowerDistribution(0, ModuleType.kCTRE);
 
     Logger.start();
